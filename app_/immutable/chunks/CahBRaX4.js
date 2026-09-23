35684ac6122a6e314b270352155a8c213e308051
KaiiDesu <kmadvincula@udm.edu.ph>
2026-09-23 10:20:27 +0800

    Updates

---
+++ app_/immutable/chunks/CahBRaX4.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bp3li2?.base??"/my-freelance-website",e=globalThis.__sveltekit_bp3li2?.assets??s;export{e as a,s as b};
--- app_/immutable/chunks/CcdiQmYV.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1mnhvh5?.base??"/my-freelance-website",e=globalThis.__sveltekit_1mnhvh5?.assets??s;export{e as a,s as b};
